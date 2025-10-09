8ae2edef93d202d08b692a5c5b4767a7fbf5f586
jeretamargo <jeretamargo01@gmail.com>
2025-10-09 08:59:17 -0300

    sincornizacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <div>
       <h1 className="text-red-500">FUNCIONANDO</h1>
+      <Footer></Footer>
     </div>
   );
 }
