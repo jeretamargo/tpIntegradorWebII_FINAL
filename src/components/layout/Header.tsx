888ee8f7136101f0af775ea9eb0aa7e3765bc936
jeretamargo <jeretamargo01@gmail.com>
2025-12-01 19:13:58 -0300

    barra de busqueda en el medio en desktop (resoluciones mayores a hd)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -18,28 +18,28 @@ function Header({ categories }: Props) {
   const { searchText, setSearchText } = useContext(SearchContext);
 
   return (
-    <header className="bg-gray-200 sticky top-0 z-50">
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-20 items-center justify-between">
+    <header className="bg-gray-200 sticky top-0 z-50   ">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8   xl:justify-items-center gap-50 ">
+        <div className="flex h-20 items-center  justify-items-center  place-content-around">
           {/* Logo */}
-          <div className="flex-1 md:flex md:items-center md:gap-12 max-h-20 items-center">
+          <div className="flex-1 md:flex md:items-center md:gap-12 max-h-20 items-center xl:mr-25">
             <Link to="/">
               <span className="sr-only">Home</span>
               <img src={logo} className="max-h-20 max-w-48" alt="Logo" />
             </Link>
           </div>
           {/* Barra de búsqueda desktop */}
-          <div className="hidden md:block md:flex-1 md:mx-4">
+          <div className="hidden md:block md:flex-1 md:mx-4 ">
             <input
               type="text"
               placeholder="Buscá tu producto"
               value={searchText}
               onChange={(e) => setSearchText(e.target.value)}
-              className="w-full p-2 border border-gray-600 shadow-md bg-gray-300  rounded-lg shadow focus:outline-none focus:ring focus:ring-indigo-100"
+              className=" w-full 2xl:w-160 xl:w-100 p-2 border border-gray-600 shadow-md bg-gray-300  rounded-lg flex flex-nowrap focus:outline-none focus:ring focus:ring-indigo-100"
             />
           </div>
           {/* Menú + Carrito */}
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-4 xl:ml-25">
             {/* Categorías (desktop) */}
             <div className="hidden md:relative md:block">
               <button
@@ -51,27 +51,27 @@ function Header({ categories }: Props) {
                 Categorías
               </button>
 
-  {showCategories && (
-    <div
-      className="absolute end-0 z-10 mt-0.5 w-56 divide-y divide-gray-100 rounded-md border border-gray-100 bg-white shadow-lg"
-      role="menu"
-    >
-      <div className="p-2">
-        {categories.map((cat) => (
-          <Link
-            key={cat.id}
-            to={`/category/${cat.id}`}
-            onClick={() => setShowCategories(false)} // cierra el menú al hacer click
-            className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
-            role="menuitem"
-          >
-            {cat.title}
-          </Link>
-        ))}
-      </div>
-    </div>
-  )}
-</div>
+              {showCategories && (
+                <div
+                  className="absolute end-0 z-10 mt-0.5 w-56 divide-y divide-gray-100 rounded-md border border-gray-100 bg-white shadow-lg"
+                  role="menu"
+                >
+                  <div className="p-2">
+                    {categories.map((cat) => (
+                      <Link
+                        key={cat.id}
+                        to={`/category/${cat.id}`}
+                        onClick={() => setShowCategories(false)} // cierra el menú al hacer click
+                        className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+                        role="menuitem"
+                      >
+                        {cat.title}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
 
             {/* Botón hamburguesa (mobile) */}
             <div className="block md:hidden">
@@ -97,8 +97,11 @@ function Header({ categories }: Props) {
             </div>
 
             {/* Carrito */}
-            <button className="relative cursor-pointer" onClick={toggleCart}>
-              <img src={cartIcon} alt="Cart" className="w-8 cursor-pointer" />
+            <button
+              className="relative cursor-pointer flex-shrink-0 flex"
+              onClick={toggleCart}
+            >
+              <img src={cartIcon} alt="Cart" className="w-10 cursor-pointer" />
               <span className="text-center absolute bottom-0 right-0 z-10 bg-red-600/90 rounded-full h-5 w-5 text-white text-xs flex items-center justify-center">
                 {totalQuantity}
               </span>
@@ -108,30 +111,30 @@ function Header({ categories }: Props) {
       </div>
 
       {/* Menú mobile desplegable */}
-{showMobileMenu && (
-  <div className="absolute left-0 right-0 top-16 z-20 bg-gray-200 border-t border-gray-200 shadow-md">
-    <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-       <header>
-        <h2 className="text-xl text-center py-4 font-bold text-gray-900 sm:text-2xl">
-          Buscá por categorías!
-        </h2>
-      </header>
-      <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4 justify-items-center">
-        {categories.map((cat) => (
-          <li key={cat.id} className="w-full flex justify-center">
-            <Link
-              to={`/category/${cat.id}`}
-              onClick={() => setShowMobileMenu(false)} // cierra el menú
-              className="inline-block w-full text-center rounded-sm bg-[#2892d7] px-4 py-2 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"
-            >
-              {cat.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </div>
-)}
+      {showMobileMenu && (
+        <div className="absolute left-0 right-0 top-16 z-20 bg-gray-200 border-t border-gray-200 shadow-md">
+          <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+            <header>
+              <h2 className="text-xl text-center py-4 font-bold text-gray-900 sm:text-2xl">
+                Buscá por categorías!
+              </h2>
+            </header>
+            <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4 justify-items-center">
+              {categories.map((cat) => (
+                <li key={cat.id} className="w-full flex justify-center">
+                  <Link
+                    to={`/category/${cat.id}`}
+                    onClick={() => setShowMobileMenu(false)} // cierra el menú
+                    className="inline-block w-full text-center rounded-sm bg-[#2892d7] px-4 py-2 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"
+                  >
+                    {cat.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
